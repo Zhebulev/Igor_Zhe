efc65bbed345ea03e54c64ec6308a2b6cff77d46
Igor A.Zhebulev <98885019+Zhebulev@users.noreply.github.com>
2022-04-24 14:34:07 +0300

    Update search-file_611.py

--- search-file_611.py
+++ search-file_611.py
@@ -1,5 +1,4 @@
 import os
-
 stroka = ''
 stroka = input('введите, что вы ищите в папке "офис_611": ')
 g = [os.path.join(z, i)
@@ -9,11 +8,6 @@ print('\nвот что сколько нашёл: ', len(g),'\n')
 for i in g:
     print(i,'\n')
 
-#os.startfile("C:/Users/zhebulev.i/Desktop/1Py/oggo.txt", "print")
-
-##C:\Users\zhebulev.i\Desktop\1Py>pyinstaller --onefile discont.py
-    
-    
 
   
             
